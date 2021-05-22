44f93179de76c63d680691aab8fb4d53a647246d
Victor Saldanha Nunes <victorsnunes99@gmail.com>
2021-05-22 23:38:01 +0100

    int7.sql

--- Projeto/int7.sql
+++ Projeto/int7.sql
@@ -1,8 +1,25 @@
 --- Porcentagem ingressos vendidos em cada evento---
-SELECT Nome, 
-  (
-      (SELECT COUNT(*) FROM ((Evento JOIN Ingresso ON Evento.ID = EventoID) WHERE (Evento.Nome = Nome))) 
-      +
-      (SELECT COUNT(*) FROM ((Evento JOIN PrecoAssento ON Evento.ID = EventoID) WHERE (Evento.Nome = Nome)))
- ) AS PorcentagemVendidos
-FROM Evento;
+
+DROP VIEW IF EXISTS IngressosVendidos;
+DROP VIEW IF EXISTS AssentosDisponiveis;
+
+CREATE VIEW IngressosVendidos AS
+SELECT Nome AS Evento, COUNT(*) AS QtdeIngressosVendidos
+FROM (Ingresso JOIN PrecoAssento USING (EventoID, Lugar)
+               JOIN Evento ON Evento.ID = EventoID) EventoIngressosVendidos
+GROUP BY EventoID;
+
+CREATE VIEW AssentosDisponiveis AS
+SELECT Nome AS Evento, COUNT(*) AS QtdeAssentos
+FROM (PrecoAssento JOIN Evento ON Evento.ID = EventoID)
+GROUP BY EventoID;
+
+
+
+SELECT Evento, CAST(QtdeIngressosVendidos AS FLOAT) / CAST(QtdeAssentos AS FLOAT) AS PorcentagemIngressosVendidos
+FROM AssentosDisponiveis NATURAL JOIN IngressosVendidos;
+
+
+
+DROP VIEW IngressosVendidos;
+DROP VIEW AssentosDisponiveis;
