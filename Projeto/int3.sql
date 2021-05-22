419fb5dd05ed8911e6cd7eeb54ce5e1a3aa4cf0d
Victor Saldanha Nunes <victorsnunes99@gmail.com>
2021-05-22 23:59:17 +0100

    int3.sql

--- Projeto/int3.sql
+++ Projeto/int3.sql
@@ -1,6 +1,5 @@
 ---- Porcentagem de artistas que possuem um empresario ----
 
 SELECT CAST((SELECT COUNT(*)
-             FROM Artista JOIN AssessoriaArtista ON PessoaID = ArtistaID) AS FLOAT)
-       /
-       CAST((SELECT COUNT(*) FROM Artista) AS FLOAT);
+             FROM Artista JOIN AssessoriaArtista ON PessoaID = ArtistaID) AS FLOAT) / 
+       CAST((SELECT COUNT(*) FROM Artista) AS FLOAT) AS PorcentagemArtistasComEmpresario ;
